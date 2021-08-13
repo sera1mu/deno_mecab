fd6ede6958f4f64672f0f08d8349d0da4aaaa875
sera1mu <ifrea0281@gmail.com>
2021-08-13 17:32:17 +0900

    Add comments

--- MeCab.ts
+++ MeCab.ts
@@ -7,11 +7,19 @@ export class MeCab {
   private cmd: string[];
   private options?: MeCabOptions;
 
+  /**
+   * @param cmd Command (Usually the path to the executable)
+   * @param options Running options
+   */
   constructor(cmd: string[], options?: MeCabOptions) {
     this.cmd = cmd;
     this.options = options;
   }
 
+  /**
+   * Run MeCab(cmd)
+   * @param cmdArgs After cmd args (e.g. -Ochasen)
+   */
   private async runMeCab(text: string, cmdArgs?: string[]): Promise<string> {
     const options: Deno.RunOptions = {
       cmd: (cmdArgs ? this.cmd.concat(cmdArgs) : this.cmd),
@@ -21,14 +29,18 @@ export class MeCab {
       stdin: "piped",
     };
 
+    // Initialize TextEncoder and TextDecoder
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
 
+    // Create process
     const process = Deno.run(options);
 
+    // Write text to stdin
     await process.stdin?.write(encoder.encode(text));
     process.stdin?.close();
 
+    // Get status and stdout
     const [{ code }, stdout] = await Promise.all([
       process.status(),
       process.output(),
@@ -39,15 +51,22 @@ export class MeCab {
         throw stdout;
     }
 
+    // Return process stdout
     const decodedOutput = decoder.decode(stdout);
     return decodedOutput;
   }
 
+  /**
+   * Parse (morphological analysis) text
+   */
   async parse(text: string): Promise<string[][]> {
+    // Run MeCab
     const result = await this.runMeCab(text);
+    // Remove not needed symbol
     const splitedResult = result.replace(/\nEOS\n/, '').split("\n");
     const parsedResult = [];
 
+    // Edit result
     for(const line of splitedResult) {
       const splitedLine = line.replace(/\t/, ",").split(",");
       parsedResult.push(splitedLine);
@@ -56,11 +75,18 @@ export class MeCab {
     return parsedResult;
   }
 
+  /**
+   * Get a dump of text
+   */
   async dump(text: string): Promise<string[][]> {
+    // Run MeCab
     const result = await this.runMeCab(text, ["-Odump"]);
+
+    // Remove not needed symbol
     const splitedResult = result.replace(/\n$/, '').split("\n");
     const parsedResult = [];
 
+    // Edit result
     for(const line of splitedResult) {
       const splitedLine = line.split(" ");
       const splitedLineFeature: string[] = splitedLine[2].split(",");
@@ -72,11 +98,18 @@ export class MeCab {
     return parsedResult;
   }
 
+  /**
+   * Parse (morphological analysis) text in Chasen compatible
+   * @param includeSpaces Whether to ignore half-width spaces
+   */
   async chasen(text: string, includeSpaces?: boolean): Promise<string[][]> {
+    // Run MeCab
     const result = await this.runMeCab(text, includeSpaces ? ["-Ochasen2"] : ["-Ochasen"]);
+    // Remove not needed symbol
     const splitedResult = result.replace(/\nEOS\n/, '').split("\n");
     const parsedResult = [];
 
+    // Edit result
     for(const line of splitedResult) {
       const splitedLine = line.replace(/\t/g, ",").split(",");
       parsedResult.push(splitedLine);
@@ -85,11 +118,17 @@ export class MeCab {
     return parsedResult;
   }
 
+  /**
+   * Text parsing (morphological analysis) and outputting only morphemes and part of speech
+   */
   async simple(text: string): Promise<string[][]> {
+    // Run MeCab
     const result = await this.runMeCab(text, ["-Osimple"]);
+    // Remove not needed symbol
     const splitedResult = result.replace(/\nEOS\n/, '').split("\n");
     const parsedResult = [];
 
+    // Edit result
     for(const line of splitedResult) {
       const splitedLine = line.replace(/\t/, ",").split(",");
       parsedResult.push(splitedLine);
@@ -98,28 +137,38 @@ export class MeCab {
     return parsedResult;
   }
 
+  /**
+   * Word-separate text
+   */
   async wakati(text: string): Promise<string[]> {
     let result: string;
     try {
+      // Run MeCab
       result = await this.runMeCab(text, ['-Owakati']);
     } catch(err) {
       throw new Error(`Failed to run MeCab correctly: ${err.message}`);
     }
 
+    // Edit result
     const editedResult = result.split(" ");
     editedResult.pop();
 
     return editedResult;
   }
 
+  /**
+   * Add reading to text
+   */
   async yomi(text: string): Promise<string> {
     let result: string;
     try {
+      // Run MeCab
       result = await this.runMeCab(text, ['-Oyomi']);
     } catch(err) {
       throw new Error(`Failed to run MeCab correctly: ${err.message}`);
     }
 
+    // Edit result
     const editedResult = result.replace(/\n/g, "");
     return editedResult;
   }
