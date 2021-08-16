46886b3acba5e51aaa19ac99265967544cc59c25
sera1mu <ifrea0281@gmail.com>
2021-08-16 10:55:23 +0900

    Add example script

---
+++ example.ts
@@ -0,0 +1,23 @@
+import { MeCab } from "./mod.ts";
+
+const mecab = new MeCab(["mecab"]);
+
+const text = "JavaScriptはとても楽しいです。";
+
+// Parse (形態素解析)
+console.log(await mecab.parse(text));
+
+// Dump (ダンプ出力)
+console.log(await mecab.dump(text));
+
+// Chasen (Chasen互換)
+console.log(await mecab.chasen(text));
+
+// Simple (品詞のみ出力)
+console.log(await mecab.simple(text));
+
+// Wakati (わかち書き)
+console.log(await mecab.wakati("JavaScriptはとても楽しいです。"));
+
+// Yomi (読み付与)
+console.log(await mecab.yomi("日本語"));
